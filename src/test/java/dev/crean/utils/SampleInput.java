64e385546e72c24cb794b20799c6e8d2969ee4ef
Charlie Crean <charliecrean85@gmail.com>
2025-12-10 11:38:39 +0000

    Add GitHub action for building changes (#5)
    * Update JDK version from 17 to 21 in workflow
    
    * Downgrade jdk version
    
    * Disable tests for CI since they use private AOC files
    
    * Revert unintended change

--- src/test/java/dev/crean/utils/SampleInput.java
+++ src/test/java/dev/crean/utils/SampleInput.java
@@ -10,5 +10,4 @@ public class SampleInput extends BaseFileInput {
     public static final String DAY_FIVE = buildPath(DIR, FIVE);
     public static final String DAY_SIX = buildPath(DIR, SIX);
     public static final String DAY_SEVEN = buildPath(DIR, SEVEN);
-
 }
